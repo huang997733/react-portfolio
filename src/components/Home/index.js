1dd3e22744cc2397eb01ffeb4d9fdb0141d4a252
huang997733 <williamhzy99@gmail.com>
2022-12-22 16:48:29 +1100

    Building home page

---
+++ src/components/Home/index.js
@@ -0,0 +1,43 @@
+import { useState } from 'react'
+import { Link } from 'react-router-dom'
+import LogoTitle from '../../assets/images/logo-Z.png'
+import AnimatedLetters from '../AnimatedLetters'
+import './index.scss'
+
+const Home = () => {
+  const [letterClass, setLetterClass] = useState('text-animate')
+  const nameArray = ['h', 'u', 'a', 'n', 'g']
+  const jobArray = ['d', 'e', 'v', 'e', 'l', 'o', 'p', 'e', 'r', '.']
+
+  return (
+    <div className="container home-page">
+      <div className="text-zone">
+        <h1>
+          <span className={letterClass}>H</span>
+          <span className={`${letterClass} _12`}>i,</span>
+          <br />
+          <span className={`${letterClass} _13`}>I</span>
+          <span className={`${letterClass} _14`}>'m</span>
+          <img src={LogoTitle} alt="developer" />
+          <AnimatedLetters
+            letterClass={letterClass}
+            strArray={nameArray}
+            idx={15}
+          />
+          <br />
+          <AnimatedLetters
+            letterClass={letterClass}
+            strArray={jobArray}
+            idx={20}
+          />
+        </h1>
+        <h2>Computer Vision / Machine Learning Engineer / Software Engineer</h2>
+        <Link to="/contact" className="flat-button">
+          CONTACT ME
+        </Link>
+      </div>
+    </div>
+  )
+}
+
+export default Home
